27d9cac2e7aa927285000cf1f826cc88c32d8f4f
Amar <amarnath.arjsd@gmail.com>
2024-11-27 07:21:52 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_Bot',
+    version= '0.0.0',
+    author= 'Amarnath',
+    author_email= 'amarnath.arjsd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
